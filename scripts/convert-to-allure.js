d1943c7222aaa891f91e89bb6ccb89095be62f0b
shigeyasushimamura <sgys.mtk@gmail.com>
2025-11-16 03:01:33 +0900

    DDIA

--- scripts/convert-to-allure.js
+++ scripts/convert-to-allure.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 const testResults = JSON.parse(fs.readFileSync("./test-results.json", "utf8"));
 const allureResultsDir = "./allure-results";
